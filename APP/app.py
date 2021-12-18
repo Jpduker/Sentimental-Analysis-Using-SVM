2104d4b7d86c991c0b9f94f0f1ee2aa430f34c09
jpduker <jayaprakashnis619@gmail.com>
2021-12-18 16:25:35 +0530

    commit

--- APP/app.py
+++ APP/app.py
@@ -8,6 +8,33 @@ import numpy as np
 import joblib
 
 # function to connect with the model
+pipeline = joblib.load(
+    open("model/Emotion_classification_pipeline_17_dec_2021.pkl", "rb")
+)
+
+
+def predict_emotions(docx):
+    results = pipeline.predict([docx])
+    return results
+
+
+def get_prediction_probability(docx):
+    results = pipeline.predict_proba([docx])
+    return results
+
+
+emotions_emoji_dict = {
+    "anger": "😠",
+    "disgust": "🤮",
+    "fear": "😨😱",
+    "happy": "🤗",
+    "joy": "😂",
+    "neutral": "😐",
+    "sad": "😔",
+    "sadness": "😔",
+    "shame": "😳",
+    "surprise": "😮",
+}
 
 
 def main():
@@ -24,15 +51,20 @@ def main():
             col1, col2 = st.columns(2)
 
             # applying the functions
+            prediction = predict_emotions(raw_text)
+            probability = get_prediction_probability(raw_text)
 
             with col1:
                 st.success("Original Text")
                 st.write(raw_text)
 
                 st.success("Prediction")
+                emoji_icon = emotions_emoji_dict[prediction]
+                st.write("{}:{}".format(prediction, emoji_icon))
 
             with col2:
                 st.success("Prediction Probability")
+                st.write(probability)
 
     elif choice == "Monitor":
         st.subheader("Monitor-App")
