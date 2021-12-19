3fceb1832fe26562d1b6386b3d5bf2bc704572fd
jpduker <jayaprakashnis619@gmail.com>
2021-12-19 13:17:40 +0530

    commit

--- APP/app.py
+++ APP/app.py
@@ -1,24 +1,25 @@
-# importing core packages
+# Core Packages
 import streamlit as st
+import altair as alt
 
-# Importing EDA packages
+# Exploratory data analysis Packages
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
+# Utils
 import joblib
 
-# function to connect with the model
 pipeline = joblib.load(
     open("model/Emotion_classification_pipeline_17_dec_2021.pkl", "rb")
 )
-
-
+# Function to connect with our ML model
 def predict_emotions(docx):
     results = pipeline.predict([docx])
-    return results
+    return results[0]
 
 
-def get_prediction_probability(docx):
+def get_prediction_proba(docx):
     results = pipeline.predict_proba([docx])
     return results
 
@@ -36,23 +37,26 @@ emotions_emoji_dict = {
     "surprise": "😮",
 }
 
-
+# Main Application
 def main():
-    menu = ["Home", "monitor", "About"]
+    st.title("Emotion Classifier App")
+    menu = ["Home"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
-        st.subheader("Emotion Analysis App")
+        ("Home", datetime.now())
+        st.subheader("Emotion In Text")
 
         with st.form(key="emotion_clf_form"):
             raw_text = st.text_area("Type Here")
             submit_text = st.form_submit_button(label="Submit")
+
         if submit_text:
             col1, col2 = st.columns(2)
 
-            # applying the functions
+            # Apply the linkage function here
             prediction = predict_emotions(raw_text)
-            probability = get_prediction_probability(raw_text)
+            probability = get_prediction_proba(raw_text)
 
             with col1:
                 st.success("Original Text")
@@ -61,16 +65,22 @@ def main():
                 st.success("Prediction")
                 emoji_icon = emotions_emoji_dict[prediction]
                 st.write("{}:{}".format(prediction, emoji_icon))
+                st.write("Confidence:{}".format(np.max(probability)))
 
             with col2:
                 st.success("Prediction Probability")
-                st.write(probability)
+                # st.write(probability)
+                proba_df = pd.DataFrame(probability, columns=pipeline.classes_)
+                # st.write(proba_df.T)
+                proba_df_clean = proba_df.T.reset_index()
+                proba_df_clean.columns = ["emotions", "probability"]
 
-    elif choice == "Monitor":
-        st.subheader("Monitor-App")
-
-    else:
-        st.subheader("About")
+                fig = (
+                    alt.Chart(proba_df_clean)
+                    .mark_bar()
+                    .encode(x="emotions", y="probability", color="emotions")
+                )
+                st.altair_chart(fig, use_container_width=True)
 
 
 if __name__ == "__main__":
